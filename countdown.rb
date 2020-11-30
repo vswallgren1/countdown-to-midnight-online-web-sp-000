3294a4f071841dd8ac0367694d9ab446ce65b814
Vince W <vincewallgren@me.com>
2020-11-30 05:32:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ end
 
  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  number = 10
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep(5)
+end
+  "Happy NEW YEAR!"
+end
