27dd0c639b5d43e415554776c724b7b4732381e8
Vince W <vincewallgren@me.com>
2020-11-30 05:31:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,9 @@
-#write your code here
-
-def countdown
+def countdown(number)
   number = 10
-
   while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+  puts "#{number} SECOND(S)!"
+  number -= 1
 end
-"HAPPY NEW YEAR!"
+
+ "HAPPY NEW YEAR!"
 end
